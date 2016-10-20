6b5dc7dfea28058054d32fb53a4b336c308e4650
ChenAllen0305 <chel1_15@uni.worc.ac.uk>
2016-10-20 14:25:33 +0100

    update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+var key = "pen";
+var value = "apple";
+
+window.localStorage.setItem(key, value);
+
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready");
@@ -13,7 +18,7 @@ var launched_count = 0;
     // device APIs are available
     //
     function onDeviceReady() {
-		alert("device ready");
+		alert("apple pen");
         
         
     }
